1d86faf86994e5ba19a0f341d3d674d06bc5e1a1
Naoki Mizuno <mizuno.naoki@rm.is.tohoku.ac.jp>
2017-12-14 20:16:34 +0900

    Fix script to publish /initialpose only on receiving the goal

--- nodes/initialpose_publisher.py
+++ nodes/initialpose_publisher.py
@@ -4,6 +4,14 @@ import rospy
 import tf2_ros
 
 from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import PoseStamped
+
+import sys
+
+
+if __name__ != '__main__':
+    sys.stderr('This program needs to be run as a script')
+    exit(1)
 
 rospy.init_node('initialpose_publisher')
 
@@ -13,21 +21,29 @@ tf_l = tf2_ros.TransformListener(buf)
 map_frame_id = rospy.get_param('~map_frame_id', 'map')
 base_frame_id = rospy.get_param('~base_frame_id', 'base_link')
 topic_name = rospy.get_param('~topic_name', 'initialpose')
-rate = rospy.Rate(rospy.get_param('~hz', 10))
+
+
+def goal_sub(msg):
+    publish_pose(map_frame_id, base_frame_id)
+
 
 initialpose_pub = rospy.Publisher(topic_name, PoseWithCovarianceStamped, queue_size=1)
+goal_sub = rospy.Subscriber('move_base_simple/goal', PoseStamped, goal_sub, queue_size=1)
 
 
-def publish_pose(map_frame_id, base_frame_id, topic_name):
+def publish_pose(map_frame_id, base_frame_id):
     global buf
 
-    try:
-        transform = buf.lookup_transform(map_frame_id,
-                                         base_frame_id,
-                                         rospy.Time(0),
-                                         rospy.Duration(0.05))
-    except tf2_ros.TransformException:
-        return
+    while True:
+        try:
+            transform = buf.lookup_transform(map_frame_id,
+                                             base_frame_id,
+                                             rospy.Time(0),
+                                             rospy.Duration(3))
+            break
+        except tf2_ros.TransformException as e:
+            rospy.logerr(e)
+            continue
 
     pose = PoseWithCovarianceStamped()
     pose.header = transform.header
@@ -41,15 +57,5 @@ def publish_pose(map_frame_id, base_frame_id, topic_name):
     p.orientation.w = transform.transform.rotation.w
     initialpose_pub.publish(pose)
 
-if __name__ != '__main__':
-    sys.stderr('This program needs to be run as a script')
-    exit(1)
 
-while not rospy.is_shutdown():
-    try:
-        publish_pose(map_frame_id, base_frame_id, topic_name)
-        rate.sleep()
-    except rospy.exceptions.ROSInterruptException:
-        print('Bye!')
-        exit(0)
-
+rospy.spin()
